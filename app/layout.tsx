f27e9f0035151700fe516a00c3ecdf1877043d6f
Tushar Kumar <archnatushar18@gmail.com>
2024-07-12 19:34:17 +0530

    2nd commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const quicksand = Quicksand({ subsets: ["latin"],
+  weight:['300','400','500','600','700']
+ });
 
 export const metadata: Metadata = {
   title: "Deal Digger",
-  description: "Welcome to LowestPriceFinder, the premier web scraping platform designed to help you find the best deals online! Our innovative website scours the internet to bring you the lowest prices for your desired products across multiple e-commerce websites. Whether you’re shopping for electronics, clothing, home goods, or any other product, LowestPriceFinder ensures you never overpay again.",
+  description:
+    "Welcome to LowestPriceFinder, the premier web scraping platform designed to help you find the best deals online! Our innovative website scours the internet to bring you the lowest prices for your desired products across multiple e-commerce websites. Whether you’re shopping for electronics, clothing, home goods, or any other product, LowestPriceFinder ensures you never overpay again.",
 };
 
 export default function RootLayout({
@@ -16,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={quicksand.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar/>
+        {children}
+
+        </main>
+        
+        </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
 
-const page = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Buy the products at the lowest price here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
 
-export default page
+            <h1 className="head-text">
+              Find, Compare, Save
+              <span className="text-primary"> DealDigger</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve web scraping
+              tool to find the best deals online. DealDigger helps you
+              effortlessly compare prices and save more, ensuring you always get
+              the lowest prices. Discover unbeatable deals with ease and
+              transform your shopping experience with DealDigger.
+            </p>
+
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
